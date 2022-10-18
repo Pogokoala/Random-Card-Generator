440693147859e7afb9b9d99fe457c83b93d6065a
Pogokoala <pogo_33125@yahoo.com>
2022-10-18 00:56:48 +0000

    random Card officially done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let suit = ["♠️", "♥️", "♣️", "♦️"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generateRandomNumber = function() {
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
 };
+
+const generateRandomSuit = () => {
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
+const selectedSuit = generateRandomSuit();
+
+document.querySelector(".top-suit").innerHTML = selectedSuit;
+document.querySelector(".number").innerHTML = generateRandomNumber();
+document.querySelector(".bottom-suit").innerHTML = selectedSuit;
+
+if (selectedSuit === "♥️" || selectedSuit === "♦️") {
+  document.querySelector(".top-suit").style.color = "red";
+  document.querySelector(".bottom-suit").style.color = "red";
+}
